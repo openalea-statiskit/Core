181a138ea83b1dccd7aa8701d21139dc87922513
Pierre Fernique <pfernique@gmail.com>
2017-07-18 13:00:32 +0200

    Format csv files

--- src/py/statiskit/data/core/__init__.py
+++ src/py/statiskit/data/core/__init__.py
@@ -12,7 +12,7 @@ def load(*args):
     kwargs = {arg : None for arg in args}    
     if 'capushe.csv' in kwargs or len(args) == 0:
         filepath = os.path.join(os.path.dirname(__file__), "capushe.csv")
-        capushe = read_csv(filepath, header=True)
+        capushe = read_csv(filepath, header=True, sep=',')
         component = capushe.components[0]
         component.sample_space = component.sample_space.as_ordinal()
         kwargs['capushe.csv'] = capushe
