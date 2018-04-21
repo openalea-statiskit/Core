0906770368cd962fe97b0c4482069b7711a79219
Pierre Fernique <pfernique@gmail.com>
2018-04-21 12:18:31 +0200

    Update

--- bin/conda/python-statiskit_core/bld.bat
+++ bin/conda/python-statiskit_core/bld.bat
@@ -1,8 +1,8 @@
 echo ON
 
 if "%PY3K%" == "0" (
-  3to2 -n -w %SRC_DIR%\src\py\autowig
-  3to2 -n -w %SRC_DIR%\test
+  3to2 -n -w %SRC_DIR%\src\py\autowig -x str
+  3to2 -n -w %SRC_DIR%\test -x str
 )
 
 scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH%
