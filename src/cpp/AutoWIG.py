ffe163f0db6564a866140e6651f4ef07c28319ac
Pierre Fernique <pfernique@gmail.com>
2019-03-01 13:21:59 +0100

    And with relevant CI config...

--- src/cpp/AutoWIG.py
+++ src/cpp/AutoWIG.py
@@ -1,4 +1,3 @@
 def controller(asg):
     from scons_tools.site_autowig.controller.statiskit_stl import controller as stl_controller
-    asg = stl_controller(asg, library=False)
-    return asg
+    return stl_controller(asg, library=False)
