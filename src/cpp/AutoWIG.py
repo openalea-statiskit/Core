0019a67a16533f7ede218d8fa3bdffaea450c550
Pierre Fernique <pfernique@gmail.com>
2019-03-01 12:15:06 +0100

    Add without comments, don't speak about it

--- src/cpp/AutoWIG.py
+++ src/cpp/AutoWIG.py
@@ -1,7 +1,4 @@
 def controller(asg):
     from scons_tools.site_autowig.controller.statiskit_stl import controller as stl_controller
     asg = stl_controller(asg, library=False)
-    # import pdb
-    # pdb.set_trace()
-    # node = 'class ::statiskit::Selection< struct ::statiskit::DiscreteUnivariateDistribution, struct ::statiskit::DiscreteUnivariateDistributionEstimation >::Estimator'
     return asg
